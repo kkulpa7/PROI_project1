963b9598c84402bbb5656b840a85dae3c0b7772a
lo7czek <lo7czek@gmail.com>
2019-03-11 20:12:08 +0100

    Added functions to change system

--- functions.cpp
+++ functions.cpp
@@ -5,3 +5,28 @@ void number::push(string num){
 void number::show(){
     cout<<this->value;
 }
+int octToDec(string num){
+    int result=0;
+    int multiplier=1;
+    for (int a=num.length()-1; a>=0; a--){
+        result+=(num[a]-'0')*multiplier;
+        multiplier*=8;
+    }
+    return result;
+}
+string decToOct(int num){
+    string result="";
+    int divider=1;
+    char sign;
+    while(divider<=num)
+        divider*=8;
+    while(num>0 || divider!=1){
+        divider/=8;
+        sign=num/divider+'0';
+        num-=(sign-'0')*divider;
+        result+=sign;
+    }
+    if(result=="")
+        return "0";
+    return result;
+}
