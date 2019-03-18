2e66b3ab95fe98d0842dfece67443c302e45ef26
lo7czek <lo7czek@gmail.com>
2019-03-18 17:32:41 +0100

    removed all

--- include/functions.h
+++
@@ -1,5 +0,0 @@
-#include "Calculator.h"
-#ifndef FUNCTIONS_H
-#endif FUNCTIONS_H
-void menu();
-#endif
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-/*Katarzyna Kulpa 2i5
-Magic calculator (octal)
-mgr inz. Wiktor Kusmirek*/
-#include "Functions.h"
-using namespace std;
-int main(){
-    menu();
-}
